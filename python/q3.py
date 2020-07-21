faeae8df6691e14096ad0332a6d013b8f483a25f
Kofi-connection <60944282+Kofi-connection@users.noreply.github.com>
2020-07-20 23:33:12 -0400

    Update q3.py

--- python/q3.py
+++ python/q3.py
@@ -1,10 +1,10 @@
 def is_leap(year):
-  if year%4==0:
-    return True
+  if year%4!=0:
+    return False
   if year%100==0 and  (year%400!=0):
     return False
   else:
-    return False
+    return True
 
 
 
